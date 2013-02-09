a0433c681750e25d2e12779bcf63b081fcdcf1d5
René Pfeuffer <rene@pfeuffer-web.de>
2013-02-09 22:01:25 +0100

    Adopted GPS solution from http://www.javacodegeeks.com/2010/09/android-location-based-services.html

--- src/de/pfeufferweb/android/whereru/ListenActivity.java
+++ src/de/pfeufferweb/android/whereru/ListenActivity.java
@@ -1,22 +1,93 @@
 package de.pfeufferweb.android.whereru;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ListenActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_listen);
-    }
+	private static final long MINIMUM_DISTANCE_CHANGE_FOR_UPDATES = 1;
+	private static final long MINIMUM_TIME_BETWEEN_UPDATES = 1000;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_listen, menu);
-        return true;
-    }
-    
+	protected LocationManager locationManager;
+	protected Button retrieveLocationButton;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_listen);
+		retrieveLocationButton = (Button) findViewById(R.id.retrieve_location_button);
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+				MINIMUM_TIME_BETWEEN_UPDATES,
+				MINIMUM_DISTANCE_CHANGE_FOR_UPDATES, new MyLocationListener());
+
+		retrieveLocationButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				showCurrentLocation();
+			}
+		});
+	}
+
+	private void showCurrentLocation() {
+		Toast.makeText(ListenActivity.this, "Getting Location",
+				Toast.LENGTH_LONG);
+		Location location = locationManager
+				.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+		if (location != null) {
+			String message = String.format(
+					"Current Location \n Longitude: %1$s \n Latitude: %2$s",
+					location.getLongitude(), location.getLatitude());
+			Toast.makeText(ListenActivity.this, message, Toast.LENGTH_LONG)
+					.show();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_listen, menu);
+		return true;
+	}
+
+	private class MyLocationListener implements LocationListener {
+		@Override
+		public void onLocationChanged(Location location) {
+			String message = String.format(
+					"New Location \n Longitude: %1$s \n Latitude: %2$s",
+					location.getLongitude(), location.getLatitude());
+			Toast.makeText(ListenActivity.this, message, Toast.LENGTH_LONG)
+					.show();
+		}
+
+		@Override
+		public void onStatusChanged(String s, int i, Bundle b) {
+			Toast.makeText(ListenActivity.this, "Provider status changed",
+					Toast.LENGTH_LONG).show();
+		}
+
+		@Override
+		public void onProviderDisabled(String s) {
+			Toast.makeText(ListenActivity.this,
+					"Provider disabled by the user. GPS turned off",
+					Toast.LENGTH_LONG).show();
+		}
+
+		@Override
+		public void onProviderEnabled(String s) {
+			Toast.makeText(ListenActivity.this,
+					"Provider enabled by the user. GPS turned on",
+					Toast.LENGTH_LONG).show();
+		}
+	}
 }
