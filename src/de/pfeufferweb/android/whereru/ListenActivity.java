0889ba225c45639e247c1e3b243a7ff5395efe68
René Pfeuffer <rene@pfeuffer-web.de>
2013-02-10 08:50:17 +0100

    Using single update from location manager; switched long and lat in response

--- src/de/pfeufferweb/android/whereru/ListenActivity.java
+++ src/de/pfeufferweb/android/whereru/ListenActivity.java
@@ -1,20 +1,15 @@
 package de.pfeufferweb.android.whereru;
 
 import android.app.Activity;
-import android.content.Context;
-import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class ListenActivity extends Activity {
 
-	private LocationManager locationManager;
 	private Button retrieveLocationButton;
 	private EditText responseText;
 
@@ -24,29 +19,11 @@ public class ListenActivity extends Activity {
 		setContentView(R.layout.activity_listen);
 		retrieveLocationButton = (Button) findViewById(R.id.retrieve_location_button);
 		responseText = (EditText) findViewById(R.id.responseText);
-		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
 		retrieveLocationButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				showCurrentLocation();
 			}
 		});
-	}
-
-	private void showCurrentLocation() {
-		Toast.makeText(ListenActivity.this, "Getting Location",
-				Toast.LENGTH_LONG);
-		Location location = locationManager
-				.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-
-		if (location != null) {
-			String message = String.format(
-					"Current Location \n Longitude: %1$s \n Latitude: %2$s",
-					location.getLongitude(), location.getLatitude());
-			Toast.makeText(ListenActivity.this, message, Toast.LENGTH_LONG)
-					.show();
-		}
 	}
 
 	@Override
--- src/de/pfeufferweb/android/whereru/SendService.java
+++ src/de/pfeufferweb/android/whereru/SendService.java
@@ -5,8 +5,10 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.telephony.SmsManager;
 
@@ -24,7 +26,7 @@ public class SendService extends Service {
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 		String receiver = intent.getExtras().getString("receiver");
-		sendSMS(receiver, getPosition());
+		getPosition(receiver);
 		return START_NOT_STICKY;
 	}
 
@@ -33,22 +35,40 @@ public class SendService extends Service {
 		return mBinder;
 	}
 
-	private void sendSMS(String phoneNumber, String message) {
-		PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(this,
-				SmsReceiver.class), 0);
-		SmsManager sms = SmsManager.getDefault();
-		sms.sendTextMessage(phoneNumber, null, message, pi, null);
-	}
+	private void getPosition(final String receiver) {
+		locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER,
+				new LocationListener() {
 
-	private String getPosition() {
-		Location location = locationManager
-				.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+					@Override
+					public void onStatusChanged(String provider, int status,
+							Bundle extras) {
+					}
 
-		if (location != null) {
-			return String.format("Longitude: %1$s ; Latitude: %2$s",
-					location.getLongitude(), location.getLatitude());
-		} else {
-			return "unknown";
-		}
+					@Override
+					public void onProviderEnabled(String provider) {
+					}
+
+					@Override
+					public void onProviderDisabled(String provider) {
+					}
+
+					@Override
+					public void onLocationChanged(Location location) {
+						String text = (location != null) ? String.format(
+								"Latitude: %1$s ; Longitude: %2$s",
+								location.getLatitude(), location.getLongitude())
+								: "unknown";
+						sendSMS(receiver, text);
+					}
+
+					private void sendSMS(String phoneNumber, String message) {
+						PendingIntent pi = PendingIntent
+								.getActivity(SendService.this, 0, new Intent(
+										SendService.this, SmsReceiver.class), 0);
+						SmsManager sms = SmsManager.getDefault();
+						sms.sendTextMessage(phoneNumber, null, message, pi,
+								null);
+					}
+				}, null);
 	}
 }
